27d6147471708cda930e9141285d58d5708a30d2
dreeew05 <gcbulaong@up.edu.ph>
2022-03-02 12:55:32 +0800

    Lecture 1 Assignment

---
+++ Lecture1/Assignment/assignment_lec1_c4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){                                 // Problem 1: Void ==> void, add curly bracket
+    int sum;                                    // Problem 2: INT ==> int
+    /* COMPUTE RESULT */                        // Problem 3: Complete the comment, add */
+    sum = 25 + 37 - 19;                         // Problem 4: Add semicolon to the end
+    /* DISPLAY RESULTS */                       // Problem 5: // ==> */
+    printf ("The answer is %i\n", sum);          
+    
+    return 0;
+}
